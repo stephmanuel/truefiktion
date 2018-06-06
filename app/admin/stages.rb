7bdfd1c0fd9f95d4b110b6f04b0706197d463f84
Raghu Betina <raghu@firstdraft.com>
2018-06-06 19:49:29 +0000

    Whitelist columns in Stage for ActiveAdmin

--- app/admin/stages.rb
+++ app/admin/stages.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Stage do
+
+ permit_params :issue_id, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
