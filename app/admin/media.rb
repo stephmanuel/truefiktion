b6fee0a107dc0b7c43992f79344c79c41eb2eb4a
Raghu Betina <raghu@firstdraft.com>
2018-06-06 19:49:36 +0000

    Whitelist columns in Medium for ActiveAdmin

--- app/admin/media.rb
+++ app/admin/media.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Medium do
+
+ permit_params :week_id, :stage_id, :article, :video
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
