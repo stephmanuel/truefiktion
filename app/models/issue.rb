a47fcdd6126661203baeb766c93eea64a040c5a1
Raghu Betina <raghu@firstdraft.com>
2018-06-06 19:49:00 +0000

    Direct association between issues and enrollments

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,9 @@
 class Issue < ApplicationRecord
   # Direct associations
 
+  has_many   :enrollments,
+             :dependent => :destroy
+
   has_many   :stages,
              :dependent => :destroy
 
