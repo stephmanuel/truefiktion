d06b5abfa385a5e37beb90e530b31bc445425c79
Raghu Betina <raghu@firstdraft.com>
2018-06-06 19:49:32 +0000

    Whitelist columns in Week for ActiveAdmin

--- app/admin/weeks.rb
+++ app/admin/weeks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Week do
+
+ permit_params :stage_id, :issue_id, :weeks
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
