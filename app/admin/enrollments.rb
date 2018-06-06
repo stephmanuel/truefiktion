2839a6d66cae1af8f4a5c690fa4c3353b19b9a91
Raghu Betina <raghu@firstdraft.com>
2018-06-06 19:49:39 +0000

    Whitelist columns in Enrollment for ActiveAdmin

--- app/admin/enrollments.rb
+++ app/admin/enrollments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Enrollment do
+
+ permit_params :issue_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
